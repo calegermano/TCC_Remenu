74c37ab395b05282075c555e5da7f948c072ecd9
calegermano <carlosalexandre071221@gmail.com>
2025-09-15 18:40:59 -0300

    Teste backend do login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Usuario;
 use Illuminate\Support\Facades\Hash;
+use App\Models\TipoUsuario;
 
 class AuthController extends Controller
 {
@@ -17,24 +18,44 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $request->validade([
+        $request->validate([
             'email' => 'required|email',
-            'password' => 'required',
+            'senha' => 'required',
         ]);
 
         $user = Usuario::where('email', $request->email)->first();
 
-        if ($user && Hash::check($request->password, $user->senha)) {
+        if ($user && Hash::check($request->senha, $user->senha)) {
             Auth::login($user);
 
             if ($user->tipo->nome === 'admin') {
-                return redirect('/admin/dashbord');
+                return redirect('/admin/dashboard');
             } else {
                 return redirect('/home');
             }
         }
 
         return back()->withErrors(['email' => 'Email ou senha inválidos']);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'nome' =>  'required',
+            'email' => 'required|email',
+            'senha' => 'required',
+        ]);
+
+        $usuario = Usuario::create([
+            'nome' => $request->nome,
+            'email' => $request->email,
+            'senha' => bcrypt($request->senha),
+            'tipo_id' => TipoUsuario::COMUM,
+        ]);
+
+        Auth::login($usuario);
+
+        return redirect('/home');
     }
 
     public function logout()
