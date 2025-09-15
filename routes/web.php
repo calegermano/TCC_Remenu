6f5dd9af16394b8026709b7f6d57f15e7cdb6c2e
calegermano <carlosalexandre071221@gmail.com>
2025-09-15 18:40:59 -0300

    Teste backend do login

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class, 'showLoginForm']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::get('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth')->group(function () {
