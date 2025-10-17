bbcab8453e78862f0fe62b98cf5874af88aa88a4
calegermano <carlosalexandre071221@gmail.com>
2025-10-17 17:36:59 -0300

    criação das tabelas de ingredientes

---
+++ app/Http/Controllers/Ingredientes.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Ingredientes extends Controller
+{
+
+    public function IngredientesLista()
+    {
+        //lista de ingredientes
+        $ingredientes = Ingredientes::all();
+        $contadorIngredientes = $ingredientes->count();
+        if ($contadorIngredientes > 0) {
+            return response()->json ([
+                'success' => true,
+                'message' => 'Ingredientes encontrados com sucesso:',
+                'data' => $ingredientes,
+                'total' => $contadorIngredientes
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Nenhum ingrediente encontrado.'
+            ], 400);
+        }
+    }
+
+    public function CategoriasLista()
+    {
+        //lista de categorias
+        $categorias = CategoriaIngredientes::all();
+        $contadorCategorias = $categorias->count();
+        if ($contadorCategorias > 0) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Categorias encontrados com sucesso:',
+                'data' => $categorias,
+                'total' => $contadorCategorias
+            ], 200);
+        } else {
+            return response ()->json([
+                'success' => false,
+                'message' => 'Nenhuma categoria encontrada.',
+            ], 400);
+        }
+    }
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
