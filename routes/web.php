e05a988261c07d262434962f56dac302f97b1175
fabzt <santiagobinho32@gmail.com>
2025-10-28 16:32:54 -0300

    estrutura dash | problema no vendor

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashBoardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -26,12 +27,12 @@ Route::get('/conta', function () {
 Route::get('/senha', function () {
     return view('senha');
 });
+Route::get('/dashboard', [DashBoardController::class, 'index'])->middleware('auth');
+
 Route::get('/login', [AuthController::class, 'showLoginForm']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/logout', [AuthController::class, 'logout']);
-
-
 
 Route::middleware(['auth', 'isAdmin'])->group(function () {
     Route::get('/admin/dashboard', function () {
