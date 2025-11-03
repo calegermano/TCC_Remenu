2db2bd3013dcaa2b6cf552715cd0059e5f4f68ce
calegermano <carlosalexandre071221@gmail.com>
2025-11-03 18:05:19 -0300

    banco da geladeira | atualização dashbord admin | inicio da pagina de receitas

---
+++ app/Http/Controllers/GeladeiraController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Geladeira;
+use Illuminate\Http\Request;
+
+class GeladeiraController extends Controller
+{
+
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+        
+    }
+
+
+    public function show(Geladeira $geladeira)
+    {
+
+    }
+
+
+    public function update(Request $request, Geladeira $geladeira)
+    {
+        
+    }
+
+    public function destroy(Geladeira $geladeira)
+    {
+        
+    }
+}
